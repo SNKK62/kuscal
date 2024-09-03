e41a4a8193329c641c6cc4442e2247048edef54b
SNKK62 <koki.seno62@gmail.com>
2024-09-04 02:02:46 +0900

    add function call

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use nom::{
     branch::alt,
     bytes::complete::tag,
     character::complete::{alpha1, alphanumeric1, char, multispace0},
-    combinator::recognize,
+    combinator::{opt, recognize},
     error::ParseError,
     multi::{fold_many0, many0},
     number::complete::recognize_float,
@@ -29,27 +29,70 @@ fn main() {
 
     let input = "(3 + 7) / (2 + 3)";
     println!("source: {:?}, parsed: {:?}", input, ex_eval(input));
+
+    let input = "sqrt(2) / 2";
+    println!("source: {:?}, parsed: {:?}", input, ex_eval(input));
+
+    let input = "sin(pi / 4)";
+    println!("source: {:?}, parsed: {:?}", input, ex_eval(input));
+
+    let input = "atan2(1, 1)";
+    println!("source: {:?}, parsed: {:?}", input, ex_eval(input));
 }
 
 #[derive(Debug, PartialEq, Clone)]
 enum Expression<'src> {
     Ident(&'src str),
     NumLiteral(f64),
+    FnInvoke(&'src str, Vec<Expression<'src>>),
     Add(Box<Expression<'src>>, Box<Expression<'src>>),
     Sub(Box<Expression<'src>>, Box<Expression<'src>>),
     Mul(Box<Expression<'src>>, Box<Expression<'src>>),
     Div(Box<Expression<'src>>, Box<Expression<'src>>),
 }
 
+fn unary_fn(f: fn(f64) -> f64) -> impl Fn(Vec<Expression>) -> f64 {
+    move |args| {
+        f(eval(
+            args.into_iter().next().expect("function missing argument"),
+        ))
+    }
+}
+
+fn binary_fn(f: fn(f64, f64) -> f64) -> impl Fn(Vec<Expression>) -> f64 {
+    move |args| {
+        let mut args = args.into_iter();
+        let lhs = eval(args.next().expect("function missing the first argument"));
+        let rhs = eval(args.next().expect("function missing the second argument"));
+        f(lhs, rhs)
+    }
+}
+
 fn eval(expr: Expression) -> f64 {
+    use Expression::*;
     match expr {
-        Expression::Ident("pi") => std::f64::consts::PI,
-        Expression::Ident(id) => panic!("Unknown name {:?}", id),
-        Expression::NumLiteral(n) => n,
-        Expression::Add(lhs, rhs) => eval(*lhs) + eval(*rhs),
-        Expression::Sub(lhs, rhs) => eval(*lhs) - eval(*rhs),
-        Expression::Mul(lhs, rhs) => eval(*lhs) * eval(*rhs),
-        Expression::Div(lhs, rhs) => eval(*lhs) / eval(*rhs),
+        Ident("pi") => std::f64::consts::PI,
+        Ident(id) => panic!("Unknown name {:?}", id),
+        NumLiteral(n) => n,
+        FnInvoke("sqrt", args) => unary_fn(f64::sqrt)(args),
+        FnInvoke("sin", args) => unary_fn(f64::sin)(args),
+        FnInvoke("cos", args) => unary_fn(f64::cos)(args),
+        FnInvoke("tan", args) => unary_fn(f64::tan)(args),
+        FnInvoke("asin", args) => unary_fn(f64::asin)(args),
+        FnInvoke("acos", args) => unary_fn(f64::acos)(args),
+        FnInvoke("atan", args) => unary_fn(f64::atan)(args),
+        FnInvoke("atan2", args) => binary_fn(f64::atan2)(args),
+        FnInvoke("pow", args) => binary_fn(f64::powf)(args),
+        FnInvoke("exp", args) => unary_fn(f64::exp)(args),
+        FnInvoke("log", args) => binary_fn(f64::log)(args),
+        FnInvoke("log10", args) => unary_fn(f64::log10)(args),
+        FnInvoke(name, _) => {
+            panic!("Unknown function {name:?}")
+        }
+        Add(lhs, rhs) => eval(*lhs) + eval(*rhs),
+        Sub(lhs, rhs) => eval(*lhs) - eval(*rhs),
+        Mul(lhs, rhs) => eval(*lhs) * eval(*rhs),
+        Div(lhs, rhs) => eval(*lhs) / eval(*rhs),
     }
 }
 
@@ -63,7 +106,17 @@ where
 }
 
 fn factor(i: &str) -> IResult<&str, Expression> {
-    alt((number, ident, parens))(i)
+    alt((number, func_call, ident, parens))(i)
+}
+
+fn func_call(i: &str) -> IResult<&str, Expression> {
+    let (r, ident) = space_delimited(identifier)(i)?;
+    let (r, args) = space_delimited(delimited(
+        tag("("),
+        many0(delimited(multispace0, expr, space_delimited(opt(tag(","))))),
+        tag(")"),
+    ))(r)?;
+    Ok((r, Expression::FnInvoke(ident, args)))
 }
 
 fn ident(input: &str) -> IResult<&str, Expression> {
