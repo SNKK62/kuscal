84a1f1c5c5999795d1a5676ce43839c747ed16d2
SNKK62 <koki.seno62@gmail.com>
2024-09-09 02:16:09 +0900

    add var assign

--- src/main.rs
+++ src/main.rs
@@ -36,6 +36,13 @@ fn main() {
                 let value = eval(expr, &variables);
                 variables.insert(name, value);
             }
+            Statement::VarAssign(name, expr) => {
+                if !variables.contains_key(name) {
+                    panic!("Variable {name:?} is not defined");
+                }
+                let value = eval(expr, &variables);
+                variables.insert(name, value);
+            }
         }
     }
 }
@@ -55,6 +62,7 @@ enum Expression<'src> {
 enum Statement<'src> {
     Expression(Expression<'src>),
     VarDef(&'src str, Expression<'src>),
+    VarAssign(&'src str, Expression<'src>),
 }
 
 type Statements<'a> = Vec<Statement<'a>>;
@@ -67,13 +75,20 @@ fn var_def(i: &str) -> IResult<&str, Statement> {
     Ok((i, Statement::VarDef(ident, expr)))
 }
 
+fn var_assign(i: &str) -> IResult<&str, Statement> {
+    let (i, ident) = space_delimited(identifier)(i)?;
+    let (i, _) = space_delimited(tag("="))(i)?;
+    let (i, expr) = space_delimited(expr)(i)?;
+    Ok((i, Statement::VarAssign(ident, expr)))
+}
+
 fn expr_statement(i: &str) -> IResult<&str, Statement> {
     let (i, expr) = expr(i)?;
     Ok((i, Statement::Expression(expr)))
 }
 
 fn statement(i: &str) -> IResult<&str, Statement> {
-    alt((var_def, expr_statement))(i)
+    alt((var_def, var_assign, expr_statement))(i)
 }
 
 fn statements(i: &str) -> Result<Statements, nom::error::Error<&str>> {
